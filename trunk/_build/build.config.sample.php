871c3406463a63db47ccaad93437fd4222974ba1
Jason Coward <jason@modxcms.com>
2007-10-24 14:39:57 +0000

    * Ignore build.config.php files in _build directories. * Remove unnecessary variables from build.config.sample.php files.

--- trunk/_build/build.config.sample.php
+++ trunk/_build/build.config.sample.php
@@ -5,10 +5,4 @@ define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
-
-// define the connection variables
-define('XPDO_DSN', 'mysql:host=localhost;dbname=modx;charset=utf8');
-define('XPDO_DB_USER', 'root');
-define('XPDO_DB_PASS', '');
-define('XPDO_TABLE_PREFIX', 'modx_');
 ?>
