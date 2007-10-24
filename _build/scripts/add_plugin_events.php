9d6146ad75f58082d98df05fbd6d71027b6fefad
Jason Coward <jason@modxcms.com>
2007-10-24 01:29:40 +0000

    Initial import

---
+++ _build/scripts/add_plugin_events.php
@@ -0,0 +1,26 @@
+<?php
+$success= false;
+if ($pluginid= $object->get('id')) {
+    $pluginEvent= $object->xpdo->newObject('modPluginEvent');
+    $pluginEvent->set('pluginid', $pluginid);
+    $pluginEvent->set('evtid', 85);
+    $pluginEvent->set('priority', 0);
+    $success= $pluginEvent->save();
+    unset($pluginEvent);
+    
+    $pluginEvent= $object->xpdo->newObject('modPluginEvent');
+    $pluginEvent->set('pluginid', $pluginid);
+    $pluginEvent->set('evtid', 87);
+    $pluginEvent->set('priority', 0);
+    $success= $pluginEvent->save();
+    unset($pluginEvent);
+    
+    $pluginEvent= $object->xpdo->newObject('modPluginEvent');
+    $pluginEvent->set('pluginid', $pluginid);
+    $pluginEvent->set('evtid', 88);
+    $pluginEvent->set('priority', 0);
+    $success= $pluginEvent->save();
+    unset($pluginEvent);
+}
+return $success;
+?>
