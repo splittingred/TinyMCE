b9605a60efbebe7edc55ad8faef02a1f59448b25
Shaun McCormick <shaun@modxcms.com>
2007-10-24 19:36:58 +0000

    - Converted TinyMCE package builder to use modPackageBuilder.

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,4 +1,5 @@
 <?php
+
 $mtime = microtime();
 $mtime = explode(" ", $mtime);
 $mtime = $mtime[1] + $mtime[0];
@@ -14,27 +15,12 @@ require_once (MODX_CORE_PATH . 'model/modx/modx.class.php');
 $modx= new modX();
 $modx->initialize('mgr');
 //$modx->setDebug(true);
-$modx->loadClass('transport.xPDOTransport', XPDO_CORE_PATH, true, true);
-if (!$workspace= $modx->getObject('modWorkspace', array('active' => 1))) {
-    echo "\nYou must have a valid core installation with an active workspace to run the build.\n";
-    exit();
-}
-$packageDirectory= $workspace->get('path') . 'packages/';
-$packageSignature= array (
-    'name' => 'tinymce',
-    'version' => '2.0.9',
-    'release' => 'rc2',
-);
-$signature= implode('-', $packageSignature);
-$packageFilename= $signature . '.transport.zip';
-if (file_exists($packageDirectory . $packageFilename)) {
-    unlink($packageDirectory . $packageFilename);
-}
-if (file_exists($packageDirectory . $signature) && is_dir($packageDirectory . $signature)) {
-    $cacheManager= $modx->getCacheManager();
-    $cacheManager->deleteTree($packageDirectory . $signature);
-}
-$package= new xPDOTransport($modx, $signature, $packageDirectory);
+
+
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$builder = new modPackageBuilder($modx);
+$builder->create('tinymce','2.0.9','rc2');
+
 $sources= array (
     'assets' => dirname(dirname(__FILE__)) . '/assets/'
 );
@@ -43,19 +29,17 @@ $sources= array (
 // [alternative] you could also manually create the object, grabbing the source from a file
 $c= $modx->getObject('modPlugin', array ('name' => 'TinyMCE'));
 $c->set('category', 0);
-$attributes['resolve'][]= array (
-    'type' => 'php',
-    'source' => dirname(__FILE__) . '/scripts/add_plugin_events.php',
-);
-$attributes['resolve'][]= array (
-    'type' => 'file',
+$vehicle = $builder->createVehicle($c);
+$vehicle->resolve('php',array(
+	'source' => dirname(__FILE__) . '/scripts/add_plugin_events.php',
+));
+$vehicle->resolve('file',array(
     'source' => $sources['assets'] . 'plugins/tinymce',
     'target' => "return MODX_ASSETS_PATH . 'plugins/';",
-);
-$attributes['unique_key']= 'name';
-
-$package->put($c, $attributes);
-$package->pack();
+));
+$vehicle->setUniqueKey('name');
+$builder->putVehicle($vehicle);
+$builder->pack();
 
 $mtime= microtime();
 $mtime= explode(" ", $mtime);
@@ -67,3 +51,4 @@ $totalTime= sprintf("%2.4f s", $totalTime);
 echo "\nExecution time: {$totalTime}\n";
 
 exit ();
+?>
