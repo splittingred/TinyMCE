ffb0c6c7efef769d222344cb40eb3bbfdb87fbca
Shaun McCormick <shaun@modxcms.com>
2008-11-13 16:35:12 +0000

    [TinyMCE] - Fixes to get working - Added languages

---
+++ tinymce/jscripts/tiny_mce/langs/es.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({es:{
+common:{
+edit_confirm:"Desea utilizar el modo WYSIWYG para esta caja de texto?",
+apply:"Aplicar",
+insert:"Insertar",
+update:"Actualizar",
+cancel:"Cancelar",
+close:"Cerrar",
+browse:"Examinar",
+class_name:"Clase",
+not_set:"-- Not set --",
+clipboard_msg:"Copiar/Cortar/Pegar no se encuentra disponible en Mozilla y Firefox.\nDesea obtener mas informacion acerca de este tema?",
+clipboard_no_support:"Su navegador no soporta las funciones de cortapapeles, use los accesos por teclado.",
+popup_blocked:"Perdon, su bloqueo de ventanas emergentes ha deshabilitado una ventana que provee funcionalidades a la aplicacion. Necesita deshabilitar este bloqueo en este sitio para poder utilizar todas las funciones.",
+invalid_data:"Error: Se ingreso un valor invalido, estos estan marcados en rojo.",
+more_colors:"Mas colores"
+},
+contextmenu:{
+align:"Alineacion",
+left:"Izquierda",
+center:"Centro",
+right:"Derecha",
+full:"Justificado"
+},
+insertdatetime:{
+date_fmt:"%d-%m-%Y",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Insertar fecha",
+inserttime_desc:"Insertar hora",
+months_long:"Enero,Febrero,Marzo,Abril,Mayo,Junio,Julio,Agosto,Septiembre,Octubre,Noviembre,Diciembre",
+months_short:"Ene,Feb,Mar,Abr,May,Jun,Jul,Ago,Sep,Oct,Nov,Dic",
+day_long:"Domingo,Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo",
+day_short:"Dom,Lun,Mar,Mie,Jue,Vie,Sab,Dom"
+},
+print:{
+print_desc:"Imprimir"
+},
+preview:{
+preview_desc:"Vista previa"
+},
+directionality:{
+ltr_desc:"Direccion izquierda a derecha",
+rtl_desc:"Direccion derecha a izquierda"
+},
+layer:{
+insertlayer_desc:"Insertar nueva capa",
+forward_desc:"Avanzar",
+backward_desc:"Retroceder",
+absolute_desc:"Cambiar a posicion absoluta",
+content:"Nueva capa..."
+},
+save:{
+save_desc:"Guardar",
+cancel_desc:"Cancelar todos los cambios"
+},
+nonbreaking:{
+nonbreaking_desc:"Insertar caracter de espacio 'non-breaking'"
+},
+iespell:{
+iespell_desc:"Corrector ortografico",
+download:"No se detecto 'ieSpell'. Desea instalarlo ahora?"
+},
+advhr:{
+advhr_desc:"Horizontale rule"
+},
+emotions:{
+emotions_desc:"Emoticones"
+},
+searchreplace:{
+search_desc:"Buscar",
+replace_desc:"Buscar/Reemplazar"
+},
+advimage:{
+image_desc:"Insertar/editar imagen"
+},
+advlink:{
+link_desc:"Insertar/editar hipervinculo"
+},
+xhtmlxtras:{
+cite_desc:"Citation",
+abbr_desc:"Abbreviation",
+acronym_desc:"Acronym",
+del_desc:"Borrado",
+ins_desc:"Insercion",
+attribs_desc:"Insertar/Editar atributos"
+},
+style:{
+desc:"Editar Estilo CSS"
+},
+paste:{
+paste_text_desc:"Pegar como Texto plano",
+paste_word_desc:"Pegar desde Word",
+selectall_desc:"Seleccionar todo"
+},
+paste_dlg:{
+text_title:"Use CTRL+V en su teclado para pegar el texto en la ventana.",
+text_linebreaks:"Keep linebreaks",
+word_title:"Use CTRL+V en su teclado para pegar el texto en la ventana."
+},
+table:{
+desc:"Inserta una nueva tabla",
+row_before_desc:"Insertar fila (antes)",
+row_after_desc:"Insertar fila (despues)",
+delete_row_desc:"Suprimir fila",
+col_before_desc:"Insertar columna (antes)",
+col_after_desc:"Insertar columna (despues)",
+delete_col_desc:"Suprimir columna",
+split_cells_desc:"Dividir celdas",
+merge_cells_desc:"Vincular celdas",
+row_desc:"Propiedades de la fila",
+cell_desc:"Propiedades de la celda",
+props_desc:"Propiedades de la tabla",
+paste_row_before_desc:"Pegar filas (antes)",
+paste_row_after_desc:"Pegar filas (despues)",
+cut_row_desc:"Cortar fila",
+copy_row_desc:"Copiar fila",
+del:"Eliminar tabla",
+row:"Fila",
+col:"Columna",
+cell:"Celda"
+},
+autosave:{
+unload_msg:"Los cambios realizados se perderan si ud. navega fuera de esta pagina."
+},
+fullscreen:{
+desc:"Cambiar a modo Pantalla Completa"
+},
+media:{
+desc:"Insertar/editar medio embebido",
+edit:"Editar medio embebido"
+},
+fullpage:{
+desc:"Document properties"
+},
+template:{
+desc:"Insertar contenido de plantilla predefinida"
+},
+visualchars:{
+desc:"Caracteres de control ON/OFF."
+},
+spellchecker:{
+desc:"Cambiar a corrector ortografico",
+menu:"Configuracion de corrector ortografico",
+ignore_word:"Ignorar",
+ignore_words:"Ignorar todo",
+langs:"Idiomas",
+wait:"Aguarde...",
+sug:"Sugerencias",
+no_sug:"Sin sugerencias",
+no_mpell:"No se encontraron errores."
+},
+pagebreak:{
+desc:"Insertar fin de pagina."
+}}});
