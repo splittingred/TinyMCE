95349d5610806f1ed6d42ea2700ad4ab5cac1273
Shaun McCormick <shaun@modxcms.com>
2010-07-13 09:37:49 -0500

    Removing unnecessary code

--- assets/components/tinymce/tiny.js
+++ assets/components/tinymce/tiny.js
@@ -96,9 +96,3 @@ MODx.afterTVLoad = function() {
 MODx.unloadTVRTE = function() {
     Tiny.onTVUnload();
 };
-
-var tinyMCETemplateList = [
-	// Name, URL, Description
-	["Simple snippet", "templates/snippet1.htm", "Simple HTML snippet."],
-	["Layout", "templates/layout1.htm", "HTML Layout."]
-];
