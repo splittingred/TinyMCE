ef2645e253d2d9202eb76a0d01d19eb59a7fba3b
Shaun McCormick <shaun@modxcms.com>
2010-09-16 22:31:52 -0500

    Optimizations to prevent loading if richtext isnt set to 1

--- core/components/tinymce/tinymce.plugin.php
+++ core/components/tinymce/tinymce.plugin.php
@@ -31,6 +31,8 @@ switch ($modx->event->name) {
                 $tiny->properties['frontend'] = true;
                 unset($def);
             }
+            if (isset($scriptProperties['resource']) && !$resource->get('richtext')) return;
+            if (!isset($scriptProperties['resource']) && !$modx->getOption('richtext_default',null,false)) return;
             $tiny->setProperties($scriptProperties);
             $html = $tiny->initialize();
             $modx->event->output($html);
