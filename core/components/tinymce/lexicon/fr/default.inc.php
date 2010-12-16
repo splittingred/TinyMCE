87b1b366af9de7af4051c414f424a76de292f4e8
Romain <romain@meltingmedia.net>
2010-12-16 10:04:27 +0100

    corrected fallback file path + french translation

--- core/components/tinymce/lexicon/fr/default.inc.php
+++ core/components/tinymce/lexicon/fr/default.inc.php
@@ -1,27 +1,44 @@
 <?php
-/*
- * Filename:       assets/plugins/tinymce/lang/francais.inc.php
- * Function:       French language file for TinyMCE.  Needs to be translated!
- * Encoding:       ISO-Latin-1
- * Author:         Jeff Whitfield
- * Date:           2006/04/07
- * Version:        2.0.5.1
- * MODx version:   0.9.2
-*/
+/**
+ * French TinyMCE language file
+ *
+ * @package tinymce
+ * @language fr
+ */
 
-include_once(dirname(__FILE__).'/english.inc.php'); // fallback for missing defaults or new additions
+include_once(dirname(dirname(__FILE__)).'/en/default.inc.php'); // fallback for missing defaults or new additions
 
-$_lang['tinymce_editor_theme_title'] = "Theme:";
-$_lang['tinymce_editor_theme_message'] = "Here you can select which theme or skin to use with the editor.";
-$_lang['tinymce_editor_custom_plugins_title'] = "Custom Plugins:";
-$_lang['tinymce_editor_custom_plugins_message'] = "Enter the plugins to use for the 'custom' theme as a comma separated list.";
-$_lang['tinymce_editor_custom_buttons_title'] = "Custom Buttons:";
-$_lang['tinymce_editor_custom_buttons_message'] = "Enter the buttons to use for the 'custom' theme as a comma separated list for each row. Be sure that each button has the required plugin enabled in the 'Custom Plugins' setting.";
-$_lang['tinymce_editor_css_selectors_title'] = "CSS selectors:";
-$_lang['tinymce_editor_css_selectors_message'] = "Here you can enter a list of selectors that should be available in the editor. Enter them as follows:<br />'displayName=selectorName;displayName2=selectorName2'<br />For instance, say you have <b>.mono</b> and <b>.smallText</b> selectors in your CSS file, you could add them here as:<br />'Monospaced text=mono;Small text=smallText'<br />Note that the last entry should not have a semi-colon after it.";
-$_lang['tinymce_settings'] = "TinyMCE Settings";
-$_lang['tinymce_theme_simple'] = "Simple";
-$_lang['tinymce_theme_advanced'] = "Advanced";
-$_lang['tinymce_theme_editor'] = "Content Editor";
-$_lang['tinymce_theme_custom'] = "Custom";
+$_lang['tiny.toggle_editor'] = 'Basculer l\'affichage de l\'éditeur : ';
+$_lang['setting_tiny.css_selectors'] = 'Sélecteurs CSS';
+$_lang['setting_tiny.css_selectors_desc'] = 'Indiquez ici une liste de sélecteurs qui doivent être disponibles dans l\'éditeur. Entrez les comme suit :<br />"NomAffiché=NomSélecteur;NomAffiché2=NomSélecteur2"<br /> Admettons par exemple que vous ayez les classes <b>.mono</b> et <b>.smallText</b> dans votre CSS, vous pouvez les ajouter comme suit : <br />"Monospaced text=mono;Small text=smallText"<br />Veuillez noter que la dernière entrée ne doit pas se terminier par un point-virgule.';
+$_lang['setting_tiny.custom_buttons1'] = 'Rangée de boutons personnalisés 1';
+$_lang['setting_tiny.custom_buttons1_desc'] = 'Indiquez les boutons à utiliser dans la première rangée, sous forme de liste séparée par une virgule. Assurez-vous que chaque bouton à son plugin requis d\'activé dans les options de « Plugins personnalisés ».';
+$_lang['setting_tiny.custom_buttons2'] = 'Rangée de boutons personnalisés 2';
+$_lang['setting_tiny.custom_buttons2_desc'] = 'Indiquez les boutons à utiliser dans la seconde rangée, sous forme de liste séparée par une virgule. Assurez-vous que chaque bouton à son plugin requis d\'activé dans les options de « Plugins personnalisés ».';
+$_lang['setting_tiny.custom_buttons3'] = 'Rangée de boutons personnalisés 3';
+$_lang['setting_tiny.custom_buttons3_desc'] = 'Indiquez les boutons à utiliser dans la troisième rangée, sous forme de liste séparée par une virgule. Assurez-vous que chaque bouton à son plugin requis d\'activé dans les options de « Plugins personnalisés ».';
+$_lang['setting_tiny.custom_buttons4'] = 'Rangée de boutons personnalisés 4';
+$_lang['setting_tiny.custom_buttons4_desc'] = 'Indiquez les boutons à utiliser dans la quatrième rangée, sous forme de liste séparée par une virgule. Assurez-vous que chaque bouton à son plugin requis d\'activé dans les options de « Plugins personnalisés ».';
+$_lang['setting_tiny.custom_buttons5'] = 'Rangée de boutons personnalisés 5';
+$_lang['setting_tiny.custom_buttons5_desc'] = 'Indiquez les boutons à utiliser dans la cinquième rangée, sous forme de liste séparée par une virgule. Assurez-vous que chaque bouton à son plugin requis d\'activé dans les options de « Plugins personnalisés ».';
+$_lang['setting_tiny.custom_plugins'] = 'Plugins personnalisés';
+$_lang['setting_tiny.custom_plugins_desc'] = 'Liste des plugins de TinyMCE à utiliser, séparés par une virgule.';
+$_lang['setting_tiny.editor_theme'] = 'Thème « éditeur »';
+$_lang['setting_tiny.template_list'] = 'Liste de modèles';
+$_lang['setting_tiny.template_list_desc'] = 'Indiquez une liste de modèles pour le plugin de modèle. Ils doivent être séparés par une virgule et leur format doit être nom:URL:description. Par exemple, MonModèle:assets/templates/modele.tpl:Mon modèle à moi (si si, le mien)';
+$_lang['setting_tiny.path_options'] = 'Options de chemin';
+$_lang['setting_tiny.path_options_desc'] = 'Soit "rootrelative", "docrelative", ou "fullpathurl".';
+$_lang['setting_tiny.theme_advanced_blockformats'] = 'Éléments bloques HTML';
+$_lang['setting_tiny.theme_advanced_blockformats_desc'] = 'Cette option doit contenir une liste de formats, séparés par une virgule, qui seront disponibles dans le menu déroulant de formats. Cette option n\'est disponible que si le thème « avancé » est utilisé.';
+
+$_lang['setting_tiny.skin'] = 'Skins TinyMCE';
+$_lang['setting_tiny.skin_desc'] = 'Cette option vous permet d\'indiquer quel skin vous souhaitez utiliser dans votre thème. Un skin est un fichier CSS qui est chargé depuis le répertoire skin de votre thème. Le thème « avancé » inclus avec TinyMCE a deux skins, appelés "default" et "o2k7". Nous avons ajouté un autre skin, sélectionné par défaut, appelé "cirkuit".';
+$_lang['setting_tiny.skin_variant'] = 'Variante de skin TinyMCE';
+$_lang['setting_tiny.skin_variant_desc'] = 'Cette option permet d\'indiquer une vartiante de skin, par exemple « argent » ou « noir ». Le skin par défaut n\'offre aucune variante alors que « o2k7 » propose les variantes "silver" ou "black". Pour le skin "cirkuit" il y a une variante nommée "silver". Lors de la création d\'un skin, plusieurs variantes peuvent être créées, en ajoutant le fichier CSS ui_[nom_de_la_variante].css aux côtés du fichier par défaut ui.css.';
+$_lang['setting_tiny.object_resizing'] = 'Redimensionnement des objets';
+$_lang['setting_tiny.object_resizing_desc'] = 'Cette option vous permet d\'activer/désactiver la possibilité de redimensionner à la volée les tableaux et les images dans Mozilla Firefox. Activé par défaut.';
+$_lang['setting_tiny.table_inline_editing'] = 'Éditeur de tableau';
+$_lang['setting_tiny.table_inline_editing_desc'] = 'Cette option vous permet d\'activer/désactiver la possibilité de redimensionner à la volée les tableaux dans Mozilla Firefox. D\'après la documentation de TinyMCE, cette option est bogguée et non « redesignable », donc désactivée par défaut.';
+$_lang['setting_tiny.template_selected_content_classes'] = 'Template Selected Content Classes';
+$_lang['setting_tiny.template_selected_content_classes_desc'] = 'Indiquez une liste de classes CSS pour le plugin de modèle. La liste doit être séparée par un espace. Chaque modèle « with one of the specified CSS classes will have its content replaced by the selected editor content when first inserted ».';
 ?>
