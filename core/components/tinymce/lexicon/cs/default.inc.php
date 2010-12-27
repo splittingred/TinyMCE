d32ed2e80e39bfc116504f44aef63867fcfb4e07
Jan Tezner <jan.tezner@gmail.com>
2010-12-27 23:44:14 +0100

    Initial Czech language translation

---
+++ core/components/tinymce/lexicon/cs/default.inc.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Czech TinyMCE language file
+ *
+ * @package tinymce
+ * @language cs
+ *
+ * @author modxcms.cz
+ * @updated 2010-12-27
+ */
+$_lang['tiny.toggle_editor'] = 'Použít WYSIWYG: ';
+$_lang['setting_tiny.css_selectors'] = 'CSS selektory';
+$_lang['setting_tiny.css_selectors_desc'] = 'Seznam selektorů dostupný ve WYSIWYG editoru. Zadávejte je v následujícím tvaru:<br />"zobrazenýNázev=nazevSelektoru;zobrazenýNázev2=nazevSelektoru2"<br />Chceme-li mít např. selektory pro třídy <b>.mono</b> a <b>.malyText</b>, které máme v CSS souboru, zadáme je tedy takto:<br />"Neproporcionální text=mono;Malý text=malyText"<br />Pamatujte, že za posledním záznamem nesmí být středník.';
+$_lang['setting_tiny.custom_buttons1'] = '1. řádek volitelných tlačítek';
+$_lang['setting_tiny.custom_buttons1_desc'] = 'Zadejte tlačítka jako čárkou oddělený seznam tlačítek pro první řádku. Ujistěte se, že každé tlačítko má vyžadovaný plugin povolen ve "Volitelné pluginy".';
+$_lang['setting_tiny.custom_buttons2'] = '2. řádek volitelných tlačítek';
+$_lang['setting_tiny.custom_buttons2_desc'] = 'Zadejte tlačítka jako čárkou oddělený seznam tlačítek pro druhou řádku. Ujistěte se, že každé tlačítko má vyžadovaný plugin povolen ve "Volitelné pluginy".';
+$_lang['setting_tiny.custom_buttons3'] = '3. řádek volitelných tlačítek';
+$_lang['setting_tiny.custom_buttons3_desc'] = 'Zadejte tlačítka jako čárkou oddělený seznam tlačítek pro třetí řádku. Ujistěte se, že každé tlačítko má vyžadovaný plugin povolen ve "Volitelné pluginy".';
+$_lang['setting_tiny.custom_buttons4'] = '4. řádek volitelných tlačítek';
+$_lang['setting_tiny.custom_buttons4_desc'] = 'Zadejte tlačítka jako čárkou oddělený seznam tlačítek pro čtvrtou řádku. Ujistěte se, že každé tlačítko má vyžadovaný plugin povolen ve "Volitelné pluginy".';
+$_lang['setting_tiny.custom_buttons5'] = '5. řádek volitelných tlačítek';
+$_lang['setting_tiny.custom_buttons5_desc'] = 'Zadejte tlačítka jako čárkou oddělený seznam tlačítek pro pátou řádku. Ujistěte se, že každé tlačítko má vyžadovaný plugin povolen ve "Volitelné pluginy".';
+$_lang['setting_tiny.custom_plugins'] = 'Volitelné pluginy';
+$_lang['setting_tiny.custom_plugins_desc'] = 'Čárkou oddělený seznam TinyMCE pluginů.';
+$_lang['setting_tiny.editor_theme'] = 'Téma editoru';
+$_lang['setting_tiny.template_list'] = 'Seznam šablon';
+$_lang['setting_tiny.template_list_desc'] = 'Zadejte seznam šablon pro šablonovací plugin. Musejí být čárkou oddělené a dodržet následující formát názevŠablony:URL:popis. Např. Moje Šablona:assets/templates/mojesablona.html:Moje vlastní šablona.';
+$_lang['setting_tiny.path_options'] = 'Možnosti cest';
+$_lang['setting_tiny.path_options_desc'] = 'Jedno z "rootrelative", "docrelative" nebo "fullpathurl".';
+$_lang['setting_tiny.theme_advanced_blockformats'] = 'HTML blokové elementy';
+$_lang['setting_tiny.theme_advanced_blockformats_desc'] = 'Čárkou oddělený seznam HTML tagů, které jsou zobrazeny v rozalovacím seznamu Formát. Tato volba je dostupná pouze pokud je použito téma "advanced".';
+
+$_lang['setting_tiny.skin'] = 'TinyMCE Skin';
+$_lang['setting_tiny.skin_desc'] = 'Pomocí této volby si můžete zvolit skin použitý ve v daném tématu editoru. Skin je v podstatě CSS soubor, který se nahráje při načtení daného tématu z jeho adresáře. Téma "advanced", které ve výchozím stavu TinyMCE obsahuje dva skiny, "default" a "o2k7". Přidali jsme ještě skin "cirkuit", který se nyní nahrává ve výchozím stavu.';
+$_lang['setting_tiny.skin_variant'] = 'TinyMCE varianta skinu';
+$_lang['setting_tiny.skin_variant_desc'] = 'Touto možností můžete zvolit variantu skinu, např. "silver" nebo "black". "default" skin neobsahuje žádnou volitelnou variantu, skin "o2k7" nabízí k výchozí variantě varianty "silver" nebo "black". Pro skin "cirkuit" máme také variantu "silver". Při vytváření skinu, lze vytvořit i jeho varianty, přidáním souboru ui_[nazev_varianty].css jako variatnu k ui.css.';
+$_lang['setting_tiny.object_resizing'] = 'Změna velikosti objektů';
+$_lang['setting_tiny.object_resizing_desc'] = 'V této volbě lze umožnit schopnost změny velikosti tabulek a obrázků přímo v textu v prohlížečích Firefox/Mozilla. Ve výchozím stavu je nastavení aktivní.';
+$_lang['setting_tiny.table_inline_editing'] = 'Přímá úprava tabulek';
+$_lang['setting_tiny.table_inline_editing_desc'] = 'Touto volbou můžete povolit přímou editaci tabulek v prohlížečích Firefox/Mozilla. V závislosti na dokumentaci k TinyMCE, je tato volba občas problémová a občas vykazuje chyby, proto je tato volba ve výchozím nastavení vypnuta.';
+$_lang['setting_tiny.template_selected_content_classes'] = 'Template Selected Content Classes';
+$_lang['setting_tiny.template_selected_content_classes_desc'] = 'Specify a list of CSS class names for the template plugin. They must be separated by spaces. Any template element with one of the specified CSS classes will have its content replaced by the selected editor content when first inserted.';
