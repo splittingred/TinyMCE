77727a862784ac21f43f7b86e6c8ad4cf1df1b49
Shaun McCormick <shaun@modx.com>
2011-03-04 16:01:53 -0600

    [#41] Add tiny.forced_root_block setting

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -5,19 +5,10 @@
  */
 $settings = array();
 
-$settings['tiny.editor_theme']= $modx->newObject('modSystemSetting');
-$settings['tiny.editor_theme']->fromArray(array(
-    'key' => 'tiny.editor_theme',
-    'value' => 'advanced',
-    'xtype' => 'textfield',
-    'namespace' => 'tinymce',
-    'area' => 'tinymce',
-),'',true,true);
-
-$settings['tiny.custom_plugins']= $modx->newObject('modSystemSetting');
-$settings['tiny.custom_plugins']->fromArray(array(
-    'key' => 'tiny.custom_plugins',
-    'value' => 'style,advimage,advlink,modxlink,searchreplace,print,contextmenu,paste,fullscreen,noneditable,nonbreaking,xhtmlxtras,visualchars,media',
+$settings['tiny.css_selectors']= $modx->newObject('modSystemSetting');
+$settings['tiny.css_selectors']->fromArray(array(
+    'key' => 'tiny.css_selectors',
+    'value' => '',
     'xtype' => 'textfield',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
@@ -68,28 +59,37 @@ $settings['tiny.custom_buttons5']->fromArray(array(
     'area' => 'tinymce',
 ),'',true,true);
 
-$settings['tiny.css_selectors']= $modx->newObject('modSystemSetting');
-$settings['tiny.css_selectors']->fromArray(array(
-    'key' => 'tiny.css_selectors',
-    'value' => '',
+$settings['tiny.custom_plugins']= $modx->newObject('modSystemSetting');
+$settings['tiny.custom_plugins']->fromArray(array(
+    'key' => 'tiny.custom_plugins',
+    'value' => 'style,advimage,advlink,modxlink,searchreplace,print,contextmenu,paste,fullscreen,noneditable,nonbreaking,xhtmlxtras,visualchars,media',
     'xtype' => 'textfield',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
 ),'',true,true);
 
-$settings['tiny.template_list']= $modx->newObject('modSystemSetting');
-$settings['tiny.template_list']->fromArray(array(
-    'key' => 'tiny.template_list',
-    'value' => '',
+$settings['tiny.editor_theme']= $modx->newObject('modSystemSetting');
+$settings['tiny.editor_theme']->fromArray(array(
+    'key' => 'tiny.editor_theme',
+    'value' => 'advanced',
     'xtype' => 'textfield',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
 ),'',true,true);
 
-$settings['tiny.use_uncompressed_library']= $modx->newObject('modSystemSetting');
-$settings['tiny.use_uncompressed_library']->fromArray(array(
-    'key' => 'tiny.use_uncompressed_library',
-    'value' => false,
+$settings['tiny.forced_root_block']= $modx->newObject('modSystemSetting');
+$settings['tiny.forced_root_block']->fromArray(array(
+    'key' => 'tiny.forced_root_block',
+    'value' => true,
+    'xtype' => 'combo-boolean',
+    'namespace' => 'tinymce',
+    'area' => 'tinymce',
+),'',true,true);
+
+$settings['tiny.object_resizing']= $modx->newObject('modSystemSetting');
+$settings['tiny.object_resizing']->fromArray(array(
+    'key' => 'tiny.object_resizing',
+    'value' => true,
     'xtype' => 'combo-boolean',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
@@ -99,15 +99,6 @@ $settings['tiny.path_options']= $modx->newObject('modSystemSetting');
 $settings['tiny.path_options']->fromArray(array(
     'key' => 'tiny.path_options',
     'value' => '',
-    'xtype' => 'textfield',
-    'namespace' => 'tinymce',
-    'area' => 'tinymce',
-),'',true,true);
-
-$settings['tiny.theme_advanced_blockformats']= $modx->newObject('modSystemSetting');
-$settings['tiny.theme_advanced_blockformats']->fromArray(array(
-    'key' => 'tiny.theme_advanced_blockformats',
-    'value' => 'p,h1,h2,h3,h4,h5,h6,div,blockquote,code,pre,address',
     'xtype' => 'textfield',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
@@ -131,20 +122,20 @@ $settings['tiny.skin_variant']->fromArray(array(
     'area' => 'tinymce',
 ),'',true,true);
 
-$settings['tiny.object_resizing']= $modx->newObject('modSystemSetting');
-$settings['tiny.object_resizing']->fromArray(array(
-    'key' => 'tiny.object_resizing',
-    'value' => true,
+$settings['tiny.table_inline_editing']= $modx->newObject('modSystemSetting');
+$settings['tiny.table_inline_editing']->fromArray(array(
+    'key' => 'tiny.table_inline_editing',
+    'value' => false,
     'xtype' => 'combo-boolean',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
 ),'',true,true);
 
-$settings['tiny.table_inline_editing']= $modx->newObject('modSystemSetting');
-$settings['tiny.table_inline_editing']->fromArray(array(
-    'key' => 'tiny.table_inline_editing',
-    'value' => true,
-    'xtype' => 'combo-boolean',
+$settings['tiny.template_list']= $modx->newObject('modSystemSetting');
+$settings['tiny.template_list']->fromArray(array(
+    'key' => 'tiny.template_list',
+    'value' => '',
+    'xtype' => 'textarea',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
 ),'',true,true);
@@ -154,6 +145,24 @@ $settings['tiny.template_selected_content_classes']->fromArray(array(
     'key' => 'tiny.template_selected_content_classes',
     'value' => '',
     'xtype' => 'textfield',
+    'namespace' => 'tinymce',
+    'area' => 'tinymce',
+),'',true,true);
+
+$settings['tiny.theme_advanced_blockformats']= $modx->newObject('modSystemSetting');
+$settings['tiny.theme_advanced_blockformats']->fromArray(array(
+    'key' => 'tiny.theme_advanced_blockformats',
+    'value' => 'p,h1,h2,h3,h4,h5,h6,div,blockquote,code,pre,address',
+    'xtype' => 'textfield',
+    'namespace' => 'tinymce',
+    'area' => 'tinymce',
+),'',true,true);
+
+$settings['tiny.use_uncompressed_library']= $modx->newObject('modSystemSetting');
+$settings['tiny.use_uncompressed_library']->fromArray(array(
+    'key' => 'tiny.use_uncompressed_library',
+    'value' => false,
+    'xtype' => 'combo-boolean',
     'namespace' => 'tinymce',
     'area' => 'tinymce',
 ),'',true,true);
