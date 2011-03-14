25e46308df1b6c560af14b94d73a3bc6fc1a9db3
Shaun McCormick <shaun@modx.com>
2011-03-14 09:51:39 -0500

    Updating version/changelog

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','TinyMCE');
 define('PKG_NAMESPACE','tinymce');
-define('PKG_VERSION','4.2.1');
+define('PKG_VERSION','4.2.2');
 define('PKG_RELEASE','pl');
 
 /* define sources */
